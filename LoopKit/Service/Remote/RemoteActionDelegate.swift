2695a847679900d14dc7c0379004d2a9f199e365
Moti Nisenson-Ken <motinis@dev.local>
2023-12-28 15:40:40 +0200

    Add bolus support for remote carbs

--- LoopKit/Service/Remote/RemoteActionDelegate.swift
+++ LoopKit/Service/Remote/RemoteActionDelegate.swift
@@ -11,6 +11,7 @@ import Foundation
 public protocol RemoteActionDelegate: AnyObject {
     func enactRemoteOverride(name: String, durationTime: TimeInterval?, remoteAddress: String) async throws
     func cancelRemoteOverride() async throws
-    func deliverRemoteCarbs(amountInGrams: Double, absorptionTime: TimeInterval?, foodType: String?, startDate: Date?) async throws
+    func deliverRemoteCarbs(amountInGrams: Double, absorptionTime: TimeInterval?, foodType: String?, startDate: Date?,
+                            giveRecommendedBolus: Bool) async throws
     func deliverRemoteBolus(amountInUnits: Double) async throws
 }
