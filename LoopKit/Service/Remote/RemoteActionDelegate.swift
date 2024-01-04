dbe60bd9df065b40d997ed4fe526f4d0c998ded9
Moti Nisenson-Ken <motinis@dev.local>
2024-01-04 13:36:47 +0200

    Add BolusType for remote carb delivery with bolus

--- LoopKit/Service/Remote/RemoteActionDelegate.swift
+++ LoopKit/Service/Remote/RemoteActionDelegate.swift
@@ -12,6 +12,17 @@ public protocol RemoteActionDelegate: AnyObject {
     func enactRemoteOverride(name: String, durationTime: TimeInterval?, remoteAddress: String) async throws
     func cancelRemoteOverride() async throws
     func deliverRemoteCarbs(amountInGrams: Double, absorptionTime: TimeInterval?, foodType: String?, startDate: Date?,
-                            giveRecommendedBolus: Bool) async throws
+                            bolusType: BolusType?) async throws
     func deliverRemoteBolus(amountInUnits: Double) async throws
+}
+
+public enum BolusType : Codable {
+    case recommended, nonCorrecting
+    
+    public func amount(_ recommendation: ManualBolusRecommendation) -> Double {
+        switch self {
+        case .recommended: return recommendation.amount
+        case .nonCorrecting: return Swift.min(recommendation.amount, recommendation.carbsAmount ?? 0)
+        }
+    }
 }
