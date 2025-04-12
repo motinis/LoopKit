69bdf9f5601bd13e493b10356417bc2073f92019
mnk <moti.nisenson@gmail.com>
2025-04-12 17:20:08 +0300

    Attempted fix to issue 2267 (basal rates view not updating)

--- LoopKitUI/ViewModels/TherapySettingsViewModel.swift
+++ LoopKitUI/ViewModels/TherapySettingsViewModel.swift
@@ -173,6 +173,7 @@ extension TherapySettingsViewModel {
     public func saveBasalRates(basalRates: BasalRateSchedule) {
         therapySettings.basalRateSchedule = basalRates
         delegate?.saveCompletion(therapySettings: therapySettings)
+        objectWillChange.send() // addresses issue 2267        
     }
     
     public func saveDeliveryLimits(limits: DeliveryLimits) {
