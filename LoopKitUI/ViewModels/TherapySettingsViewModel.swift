f2744e6589645355837f67380381e65c173a6540
mnk <moti.nisenson@gmail.com>
2025-04-17 14:27:39 +0300

    One more attempted fix to issue 2267

--- LoopKitUI/ViewModels/TherapySettingsViewModel.swift
+++ LoopKitUI/ViewModels/TherapySettingsViewModel.swift
@@ -22,6 +22,7 @@ public protocol TherapySettingsViewModelDelegate: AnyObject {
 public class TherapySettingsViewModel: ObservableObject {
     
     @Published public var therapySettings: TherapySettings
+    @Published public var updateCounter: Int = 0
     private let initialTherapySettings: TherapySettings
     let sensitivityOverridesEnabled: Bool
     let adultChildInsulinModelSelectionEnabled: Bool
@@ -171,6 +172,7 @@ extension TherapySettingsViewModel {
     }
     
     public func saveBasalRates(basalRates: BasalRateSchedule) {
+        updateCounter += 1
         therapySettings.basalRateSchedule = basalRates
         delegate?.saveCompletion(therapySettings: therapySettings)
     }
